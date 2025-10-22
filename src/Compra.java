4f5764e77939c1c6c264147b6dd7cbef7be2081b
Mikatholic <miguelangelferroescalante@gmail.com>
2025-10-21 20:17:37 -0500

    Creado el constructor para valor y descripciòn

--- src/Compra.java
+++ src/Compra.java
@@ -2,6 +2,8 @@ public class Compra {
     private double valor;
     private String descripciòn;
 
-
-
+    public Compra(double valor, String descripciòn) {
+        this.valor = valor;
+        this.descripciòn = descripciòn;
+    }
 }

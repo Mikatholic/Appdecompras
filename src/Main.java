1393f2aac3fb896e456eea4644d347891125c54a
Mikatholic <miguelangelferroescalante@gmail.com>
2025-10-22 16:31:13 -0500

    Agregando un for

--- src/Main.java
+++ src/Main.java
@@ -32,7 +32,8 @@ public class Main {
         System.out.println("***********************");
         System.out.println("COMPRAS REALIZADAS:\n");
         Collections.sort(tarjeta.getListaDeCompras());
-
-
+        for (Compra compra : tarjeta.getListaDeCompras()) {
+        System.out.println(compra.getDescripcion() + " - " +compra.getValor());
+        }
     }
 }

3639f6fe0e5553cd4a604c13df7a1c8f64ee1ad6
Mikatholic <miguelangelferroescalante@gmail.com>
2025-10-22 16:26:43 -0500

    Agregando while

--- src/Main.java
+++ src/Main.java
@@ -11,6 +11,23 @@ public class Main {
         TarjetaDeCredito tarjeta = new TarjetaDeCredito(limite);
 
         int salir = 1;
+        while(salir != 0) {
+            System.out.println("Escriba la descripcion de la compra:");
+            String descripcion = lectura.next();
 
+            System.out.println("Escriba el valor de la compra:");
+            double valor = Double.valueOf(lectura.next());
+
+            Compra compra = new Compra(valor, descripcion);
+            boolean compraRealizada = tarjeta.lanzarCompra(compra);
+
+            if (compraRealizada) {
+                System.out.println("Compra realizada!");
+                System.out.println("Escriba 0 para salir o 1 para continuar");
+                salir = lectura.nextInt();
+            } else {
+                System.out.println("Saldo insuficiente!");
+                salir = 0;
+        }
     }
 }

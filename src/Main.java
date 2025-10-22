f35677dc24f1a82e61da92f1de86cc7f3fa043f8
Mikatholic <miguelangelferroescalante@gmail.com>
2025-10-22 17:00:47 -0500

    Finalizacion y prueba del código

--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,13 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner lectura = new Scanner(System.in);
-        System.out.println("Escriba la lista de la tarjeta: ");
+        System.out.println("Escriba el límite de la tarjeta: ");
         double limite = lectura.nextDouble();
         TarjetaDeCredito tarjeta = new TarjetaDeCredito(limite);
 
         int salir = 1;
         while(salir != 0) {
-            System.out.println("Escriba la descripcion de la compra:");
+            System.out.println("Escriba la descripción de la compra:");
             String descripcion = lectura.next();
 
             System.out.println("Escriba el valor de la compra:");
@@ -27,13 +27,14 @@ public class Main {
                 System.out.println("Saldo insuficiente!");
                 salir = 0;
             }
-
         }
         System.out.println("***********************");
         System.out.println("COMPRAS REALIZADAS:\n");
         Collections.sort(tarjeta.getListaDeCompras());
         for (Compra compra : tarjeta.getListaDeCompras()) {
-        System.out.println(compra.getDescripcion() + " - " +compra.getValor());
+            System.out.println(compra.getDescripcion() + " - " +compra.getValor());
         }
+        System.out.println("\n***********************");
+        System.out.println("\nSaldo de la tarjeta: " +tarjeta.getSaldo());
     }
 }
